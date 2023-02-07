f50be3667fe0a75fade65b5e16c07ca3cb9e7481
Alexis <lexisgasa3007@gmail.com>
2023-02-07 16:15:48 +0100

    sign in skeleton

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -5,9 +5,34 @@
   <div class="container">
     <h3 class="header-title">Log In to ToDo App</h3>
     <p class="header-subtitle">Estamos en la ruta de login. Aquí deberíais crear un form con la lógica correspondiente para que este permita al usuario loguearse con su email y su contraseña. Miraros la lógica de SignUp si necesitáis inspiración :)</p>
-    <p>Dont have an account? <PersonalRouter :route="route" :buttonText="buttonText" class="sign-up-link"/></p>
-  </div>
+    <div class="sign-in-form">
+      <form action="">
+        <input
+        type="email"
+        placeholder="example@gmail.com"
+        id="email"
+        v-model="email"
+        required
+        >
+        <br>
+        <input
+        type="text"
+        class="input-field"
+        placeholder="*********"
+        id="password"
+        v-model="password"
+        required
+        >
+        <br>
+        <button 
+        type="submit"
+        >Sign in</button>
+      </form>
 
+      
+      <p>Dont have an account? <PersonalRouter :route="route" :buttonText="buttonText" class="sign-up-link"/></p>
+    </div>
+  </div>
 </template>
 
 <script setup>
